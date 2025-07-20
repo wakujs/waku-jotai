88ae6ea1a54ac42b0ce5ba2d6a6c1f1c1eafcd4a
daishi <daishi@axlight.com>
2025-07-20 18:39:53 +0900

    fix vitest alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 /// <reference types="vitest" />
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      'waku-jotai': resolve('src'),
+    },
+  },
   test: {
     environment: 'happy-dom',
     setupFiles: ['./tests/vitest-setup.ts'],
   },
-  plugins: [tsconfigPaths()],
 });
