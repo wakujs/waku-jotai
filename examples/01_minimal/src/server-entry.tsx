23cb05fc2d9ff9711924c0bc7b6c438451988524
daishi <daishi@axlight.com>
2025-12-10 09:48:48 +0900

    fix example 1

--- examples/01_minimal/src/server-entry.tsx
+++ examples/01_minimal/src/server-entry.tsx
@@ -1,10 +1,8 @@
-import { unstable_defineEntries as defineEntries } from 'waku/minimal/server';
+import adapter from 'waku/adapters/default';
 import { Slot } from 'waku/minimal/client';
-import { unstable_createAsyncIterable as createAsyncIterable } from 'waku/server';
-
 import App from './components/app';
 
-export default defineEntries({
+export default adapter({
   handleRequest: async (input, { renderRsc, renderHtml }) => {
     if (input.type === 'component') {
       return renderRsc({
@@ -13,48 +11,11 @@ export default defineEntries({
     }
     if (input.type === 'custom' && input.pathname === '/') {
       return renderHtml(
-        { App: <App name="Waku" rscParams={undefined} /> },
+        await renderRsc({ App: <App name="Waku" rscParams={undefined} /> }),
         <Slot id="App" />,
         { rscPath: '' },
       );
     }
   },
-  handleBuild: ({
-    // renderRsc,
-    // renderHtml,
-    // rscPath2pathname,
-    unstable_generatePrefetchCode,
-  }) =>
-    createAsyncIterable(async () => {
-      const moduleIds = new Set<string>();
-      const generateHtmlHead = () =>
-        `<script type="module" async>${unstable_generatePrefetchCode(
-          [''],
-          moduleIds,
-        )}</script>`;
-      const tasks = [
-        async () => ({
-          type: 'htmlHead' as const,
-          pathSpec: [],
-          head: generateHtmlHead(),
-        }),
-        // async () => ({
-        //   type: 'file' as const,
-        //   pathname: rscPath2pathname(''),
-        //   body: await renderRsc(
-        //     { App: <App name="Waku" /> },
-        //     { moduleIdCallback: (id) => moduleIds.add(id) },
-        //   ),
-        // }),
-        // async () => ({
-        //   type: 'file' as const,
-        //   pathname: '/',
-        //   body: renderHtml({ App: <App name="Waku" /> }, <Slot id="App" />, {
-        //     rscPath: '',
-        //     htmlHead: generateHtmlHead(),
-        //   }).then(({ body }) => body),
-        // }),
-      ];
-      return tasks;
-    }),
+  handleBuild: async () => {},
 });
