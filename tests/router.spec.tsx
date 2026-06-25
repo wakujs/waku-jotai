ec96b5d7bff1806c890ed378775fee21cbda2c5e
Daishi Kato <dai-shi@users.noreply.github.com>
2026-06-26 08:34:06 +0900

    chore: update dependencies (#11)
    * wip: update deps
    
    * add mock

--- tests/router.spec.tsx
+++ tests/router.spec.tsx
@@ -1,5 +1,10 @@
-import { expect, test } from 'vitest';
+import { expect, test, vi } from 'vitest';
 import { getStore, RouterProvider } from 'waku-jotai/router';
+
+vi.mock('waku/router/server', () => ({
+  unstable_getRscPath: () => '',
+  unstable_getRscParams: () => ({}),
+}));
 
 test('exports', () => {
   expect(getStore).toBeDefined();
